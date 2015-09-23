d72096e6141467a938b4fd3f8e925a5c98267d41
Marko Alerić <maleric@foi.hr>
2015-09-23 23:19:24 +0200

    user and admins listed

--- src/java/beans/UsersBean.java
+++ src/java/beans/UsersBean.java
@@ -5,7 +5,16 @@
  */
 package beans;
 
+import database.Admins;
+import database.Users;
+import facades.AdminsFacade;
+import facades.ProjectsFacade;
+import facades.UsersFacade;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 
 /**
@@ -13,13 +22,48 @@ import javax.faces.bean.SessionScoped;
  * @author Marko
  */
 @ManagedBean
-@SessionScoped
+@RequestScoped
 public class UsersBean {
+
+    @EJB
+    private AdminsFacade adminsFacade;
+    @EJB
+    private UsersFacade usersFacade;
+    private List<Users> users;
+    private List<Admins> admins;
 
     /**
      * Creates a new instance of UsersBean
      */
     public UsersBean() {
     }
-    
+
+    @PostConstruct
+    public void afterConstructor() {
+        System.out.println("TestUserConstruct");
+        refreshList();
+    }
+
+    public List<Users> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<Users> users) {
+        this.users = users;
+    }
+
+    public void refreshList() {
+        this.users = usersFacade.findAll();
+        this.admins = adminsFacade.findAll();
+
+    }
+
+    public List<Admins> getAdmins() {
+        return admins;
+    }
+
+    public void setAdmins(List<Admins> admins) {
+        this.admins = admins;
+    }
+
 }
