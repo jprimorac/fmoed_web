fa0056daebb72243003c4c15b87c5be33aac4080
Marko Alerić <maleric@foi.hr>
2015-09-24 12:18:55 +0200

    detalji projekta update

--- src/java/beans/ViewProjectBean.java
+++ src/java/beans/ViewProjectBean.java
@@ -5,9 +5,16 @@
  */
 package beans;
 
+import database.Groups;
 import database.Projects;
+import database.Reviews;
+import database.Usergroupreview;
+import database.Users;
+import database.Usersgroups;
 import facades.ProjectsFacade;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
@@ -16,29 +23,39 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import jdk.nashorn.internal.runtime.RewriteException;
 
 /**
  *
  * @author Marko
- */ 
+ */
 @ManagedBean
-@SessionScoped
+@RequestScoped
 public class ViewProjectBean {
+
     @EJB
     private ProjectsFacade projectsFacade;
-    private Projects project;
-    private boolean chosen=false;
+    private static Projects project;
+    private static double rating;
+    private boolean chosen = false;
 
     /**
      * Creates a new instance of viewProject
      */
     public ViewProjectBean() {
         checkChosen();
+        if (chosen) {
+            refreshProjectGroups();
+        }
+
     }
-    public void checkChosen(){
-        if(project !=null && project.getId()!=null)
-            chosen=true;
+
+    public void checkChosen() {
+        if (project != null && project.getId() != null) {
+            chosen = true;
+        }
     }
+
     public Projects getProject() {
         return project;
     }
@@ -54,19 +71,57 @@ public class ViewProjectBean {
     public void setChosen(boolean chosen) {
         this.chosen = chosen;
     }
-    
-    public void viewProject(Projects project){
-        this.project=project;
-        checkChosen();
+
+    private void refreshProjectGroups() {
+        rating = 0;
+        int counter = 0;
+        for (Groups group : project.getGroupsList()) {
+            for (Reviews rev : group.getReviewsList()) {
+                rating += rev.getRating();
+                counter++;
+            }
+        }
+        rating /= counter;
         System.out.println("Test");
-        System.out.println("Pr:"+project.getName());
-        
-        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
-                try {
-                    context.redirect(context.getRequestContextPath() + "/faces/admin/viewProject.xhtml");
-                } catch (IOException ex) {
-                    Logger.getLogger(LoginBean.class.getName()).log(Level.SEVERE, null, ex);
-                }
+        System.out.println("Pr:" + project.getName());
+
     }
-        
+
+    public void viewProject(Projects project) {
+        this.project = project;
+        checkChosen();
+        refreshProjectGroups();
+
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        try {
+            context.redirect(context.getRequestContextPath() + "/faces/admin/viewProject.xhtml");
+        } catch (IOException ex) {
+            Logger.getLogger(LoginBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public double getGroupAvgRating(Groups g) {
+        double groupRate = 0;
+        for (Reviews r : g.getReviewsList()) {
+            groupRate += r.getRating();
+        }
+        return groupRate / g.getReviewsList().size();
+    }
+    public List<Users> getProjectUsers(){
+        List<Users> users = new ArrayList<>();
+        for(Groups g:project.getGroupsList()){
+            for(Usersgroups ug:g.getUsersgroupsList())
+            users.add(ug.getUser());
+        }
+        return users;
+    }
+
 }
